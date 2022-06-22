bc3b4ef41f95bb1de17bc6fbf747ca6579db97f0
LauraCamargo-tech <74160797+LauraCamargo-tech@users.noreply.github.com>
2022-06-22 20:47:01 -0300

    Função de subtração

---
+++ sub.py
@@ -0,0 +1,9 @@
+def subtrai(a,b):
+	return a - b
+
+print("Digite dois numeros para subtrailos")
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+print(subtrai(num1, num2))
